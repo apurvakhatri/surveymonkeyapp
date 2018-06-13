88aab31f44925e58ab5ca9e5134ec6bc457fe638
Apurva Khatri <yellowantapps@gmail.com>
2018-06-13 14:18:56 +0530

    push to bitbucket

--- lib/web/urls.py
+++ lib/web/urls.py
@@ -6,8 +6,7 @@ from django.conf.urls import url
 urlpatterns = [
     path("", index, name="home"),
     path("user/", userdetails, name="home"),
-    path('user/<int:integrationId>/delete/', delete_integration, name='home'),
-    url(r"^(?P<path>.*)$", index, name="home"),
+    path('user/<int:integrationId>/delete/', delete_integration, name='home')
 
 
     #path("(?P<user_integration_id>)/delete/", delete_integration, name="home")
--- surveymonkeyapp/urls.py
+++ surveymonkeyapp/urls.py
@@ -21,12 +21,12 @@ from lib.web import urls as web_urls
 from lib.records.views import integrate_app_account
 
 
-urlpatterns = [\
+urlpatterns = [
+    path('', include(web_urls)),
     path('admin/', admin.site.urls),
     path("surveymonkeyauthurl/", include(records_urls)),
     path("redirecturl/", include(records_urls)),
     path("yellowantauthurl/", include(records_urls)),
     path("webhook_receiver/", include(records_urls)),
-    path("integrate_app/",integrate_app_account),
-    path('', include(web_urls))
+    path("integrate_app/", integrate_app_account)
 ]
