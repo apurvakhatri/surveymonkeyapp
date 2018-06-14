7d0e5b7f03d509f572c2dca246ef781a41cb9877
Apurva Khatri <yellowantapps@gmail.com>
2018-06-14 12:11:26 +0530

    name changed in web(urls)

--- lib/web/urls.py
+++ lib/web/urls.py
@@ -4,8 +4,8 @@ from django.conf.urls import url
 
 urlpatterns = [
     # path("", index, name="home"),
-    path("user/", userdetails, name="home"),
-    path('user/<int:integrationId>/delete/', delete_integration, name='home'),
+    path("user/", userdetails, name="user_integrations"),
+    path('user/<int:integrationId>/delete/', delete_integration, name='user_integration_delete'),
     url(r"^(?P<path>.*)$", index, name="home")
 
 ]
