0dc9460e6cd347e1760cdb850609d78803608a16
Apurva Khatri <yellowantapps@gmail.com>
2018-06-14 12:25:50 +0530

    testing

--- lib/web/urls.py
+++ lib/web/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     # path("", index, name="home"),
     path("user/", userdetails, name="user_integrations"),
     path('user/<int:integrationId>/delete/', delete_integration, name='user_integration_delete'),
-    url(r"^(?P<path>.*)$", index, name="home")
+    url(r"^(?P<path>.*)$", index, name="home"),
 
 ]
